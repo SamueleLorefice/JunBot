c7d898b60e66b52f9a103f008e7e642dda5d806b
Samuele Lorefice <aironenerowork@gmail.com>
2016-12-12 19:54:15 +0100

    prepared a basic bot

--- Jun/Program.cs
+++ Jun/Program.cs
@@ -2,6 +2,7 @@
 //Settings File specific
 using Newtonsoft.Json;
 //BotApi specific
+using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -15,9 +16,30 @@ namespace Jun
 	static class MainClass
 	{
 		static string settingsFile = "Settings.Json";
+		static Settings settings;
+
 		public static void Main (string[] args)
 		{
-			
+			//configuration down here
+			if (System.IO.File.Exists(settingsFile)){
+				//gets token and other infos
+				settings = JsonConvert.DeserializeObject<Settings>(settingsFile);
+			}else{
+				try{
+					settings = new Settings("", 0);
+					string _out = JsonConvert.SerializeObject(settings, Formatting.Indented);
+					System.IO.File.Create(settingsFile);
+					System.IO.File.WriteAllText(settingsFile, _out);
+				}catch(Exception e){
+					Console.WriteLine("Unable to create/save settings file.");
+					Environment.Exit(1);
+				}
+			}
+
+			//telegram.bot related down here
+			TelegramBotClient Bot = new TelegramBotClient(settings.token);
+			var me = Bot.GetMeAsync().Result;
+            Console.Title = me.Username;
 		}
 	}
 }
--- Jun/Settings.cs
+++ Jun/Settings.cs
@@ -4,6 +4,14 @@ namespace Jun
 {
 	public class Settings
 	{
-		public string token = {get; set;}
+		//token of the bot
+		public string token {get; set;}
+		//id of the creator
+		public long ownerID {get; set;}
+
+		public Settings(string tok, long ownID){
+			token = tok;
+			ownID = ownerID;
+		}
 	}
 }
