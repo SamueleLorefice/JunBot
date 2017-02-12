140fc46e3a78b9394577b4649e4ba6a5936a61a2
Samuele Lorefice <aironenerowork@gmail.com>
2017-02-12 18:35:07 +0100

    Refactors code

--- Jun/BotDatabase.cs
+++ Jun/BotDatabase.cs
@@ -1,38 +1,48 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace Jun {
-    class BotDatabase {[JsonProperty(PropertyName = "GroupDB", Required = Required.AllowNull)]
+
+    internal class BotDatabase {
+
+        [JsonProperty(PropertyName = "GroupDB", Required = Required.AllowNull)]
         public List<GroupEntry> GroupDB { get; set; }
+
         [JsonProperty(PropertyName = "UserDB", Required = Required.AllowNull)]
         public List<UserEntry> UserDB { get; set; }
     }
 
-    class UserEntry {
+    internal class UserEntry {
+
         [JsonProperty(PropertyName = "ID", Required = Required.Always)]
         public long ID { get; set; }
+
         [JsonProperty(PropertyName = "FirstName", Required = Required.Always)]
         public string FirstName { get; set; }
+
         [JsonProperty(PropertyName = "LastName", Required = Required.AllowNull)]
         public string LastName { get; set; }
+
         [JsonProperty(PropertyName = "NickName", Required = Required.AllowNull)]
         public string Nickname { get; set; }
+
         [JsonProperty(PropertyName = "Subscribed", Required = Required.Always)]
         public bool Subcribed { get; set; }
+
         //public bool FilthyPeasant;
     }
 
-    class GroupEntry {
+    internal class GroupEntry {
+
         [JsonProperty(PropertyName = "ID", Required = Required.Always)]
         public long ID { get; set; }
+
         [JsonProperty(PropertyName = "Name", Required = Required.Always)]
         public string GroupName { get; set; }
+
         [JsonProperty(PropertyName = "Description", Required = Required.AllowNull)]
         public string GroupDescription { get; set; }
+
         [JsonProperty(PropertyName = "Users", Required = Required.Always)]
         public List<UserEntry> UsersList { get; set; }
     }
